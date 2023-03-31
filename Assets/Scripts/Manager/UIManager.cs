46c2eaa06006d0b56be72a44812069ef9ddb1aaf
soos2075 <herotjdgus@naver.com>
2023-03-31 22:22:15 +0900

    default
    20230331 22:20 프로젝트생성

---
+++ Assets/Scripts/Manager/UIManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager
+{
+    public GameObject UI_Root
+    {
+        get
+        {
+            GameObject root = GameObject.Find("@UI_Root");
+            if (root == null)
+            {
+                root = new GameObject { name = "@UI_Root" };
+            }
+            return root;
+        }
+    }
+
+    int _sortOrder = 10;
+
+
+    public T ShowSceneUI<T>(string name)
+    {
+        if (string.IsNullOrEmpty(name))
+            name = typeof(T).Name;
+
+        GameObject go = Managers.Resource.Instantiate($"UI/Scene/{name}");
+        T sceneUI = go.GetComponent<T>();
+        go.transform.SetParent(UI_Root.transform);
+
+        return sceneUI;
+    }
+
+    public T ShowPopupUI<T>(string name)
+    {
+        if (string.IsNullOrEmpty(name))
+            name = typeof(T).Name;
+
+        GameObject go = Managers.Resource.Instantiate($"UI/Popup/{name}");
+        T popupUI = go.GetComponent<T>();
+        go.transform.SetParent(UI_Root.transform);
+
+        return popupUI;
+    }
+
+}
